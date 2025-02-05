3825a49c1a915403dc0a882aef11363737377bad
Jakub Marcinat <jakubmarcinat@gmail.com>
2025-02-05 19:34:04 +0100

    Fixed css after merge

--- pages/index.js
+++ pages/index.js
@@ -2,35 +2,15 @@ import Hero from "@/components/asd";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
-export default function Home() {
-  return (
-    <div>
-      <Hero />
-      <div className="text-red-300">asdasd</div>
-    </div>
-  );
-}
-
-export async function getServerSideProps() {
-  const productsIds = "672b2608187ad6200a382cdb";
-  await mongooseConnect();
-  const product = await Product.findById(productsIds);
-
-  return {
-    props: { product: JSON.parse(JSON.stringify(product)) },
-  };
-}
-
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
-import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
 
 export default function HomePage({ product }) {
   console.log("asdsa", product.title);
   return (
     <div>
       <Header />
+      <Hero />
       <Featured product={product} />
     </div>
   );
